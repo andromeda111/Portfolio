e39d54edd40ebb43bc0ef4dab5b65af2e4f9ad66
John <jmseymour111@gmail.com>
2017-08-31 18:20:16 -0700

    Fixed unintentionally minifed JS in app/js

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,134 @@
-$(document).ready(function(){function a(a){var t=a.attr("href");$("html, body").animate({scrollTop:$(t).offset().top-$("nav").outerHeight()},400)}function t(a,t){var s=a.parent();t>=$(a.attr("href")).offset().top-100?(o.not(a).parent().removeClass("active"),s.addClass("active")):s.removeClass("active")}var o=$(".nav-list-item > a"),s=($(".nav-list li a"),$(".nav-fixed")),c=$(".nav-list-item"),n=$(".nav-main"),e=$(".nav-title-container"),r=$(".nav-title");$("nav, .arrow-button").find("a").click(function(t){t.preventDefault(),a($(this))}),$(window).scroll(function(){var a=$(this).scrollTop();o.each(function(o){t($(this),a)}),$(window).scrollTop()>200?window.matchMedia("(max-width: 767px)").matches?(s.css({background:"rgba(255, 255, 255, 0.8)"}),c.css({color:"#000","text-shadow":"none"}),n.removeClass("nav-center"),e.css({opacity:"0",color:"#FFF"})):(s.css({background:"rgba(255, 255, 255, 0.8)"}),c.css({color:"#000","text-shadow":"none"}),n.removeClass("nav-center").addClass("nav-to-right"),e.css({opacity:"1",color:"#000"}),r.css({display:"inherit"})):window.matchMedia("(max-width: 767px)").matches?(s.css({background:"rgba(255, 255, 255, 0.0)"}),c.css({color:"#FFF","text-shadow":"1px 1px 1px rgba(0, 0, 0, 0.8)"}),n.addClass("nav-center"),e.css({opacity:"0",color:"#000"})):(s.css({background:"rgba(255, 255, 255, 0.0)"}),c.css({color:"#FFF","text-shadow":"1px 1px 1px rgba(0, 0, 0, 0.8)"}),n.removeClass("nav-to-right").addClass("nav-center"),e.css({opacity:"0",color:"#FFF"}),r.fadeOut(300))})});
-//# sourceMappingURL=maps/main.js.map
+$(document).ready(function() {
+  // Variables
+  var $allNavLinks = $('.nav-list-item > a')
+  var $navLink = $('.nav-list li a')
+  var $navFixed = $('.nav-fixed')
+  var $navListItem = $('.nav-list-item')
+  var $navMain = $('.nav-main')
+  var $navTitleContainer = $('.nav-title-container')
+  var $navTitle = $('.nav-title')
+
+  // Smooth Scroll
+  $('nav, .arrow-button').find('a').click(function(e) {
+    e.preventDefault()
+    smoothScroll($(this))
+    return
+  })
+
+// On Scroll event handlers
+  $(window).scroll(function() {
+    var y = $(this).scrollTop()
+
+    // Highlight menu item on scroll
+    $allNavLinks.each(function(event) {
+      scrollApplyHighlight($(this), y)
+      return
+    })
+
+    // HEADER / NAVBAR STYLING
+    // When Scroll is greater than 200px
+    if ($(window).scrollTop() > 200) {
+      // Small Media < 768px
+      if (window.matchMedia('(max-width: 767px)').matches) {
+
+        $navFixed.css({
+          'background': 'rgba(255, 255, 255, 0.8)'
+        })
+        $navListItem.css({
+          'color': '#000',
+          'text-shadow': 'none'
+        })
+        $navMain.removeClass("nav-center")
+        $navTitleContainer.css({
+          "opacity": "0",
+          "color": "#FFF"
+        })
+      } else {
+        // Larger media > 768px
+        $navFixed.css({
+          'background': 'rgba(255, 255, 255, 0.8)'
+        })
+        $navListItem.css({
+          'color': '#000',
+          'text-shadow': 'none'
+        })
+        $navMain.removeClass("nav-center").addClass('nav-to-right')
+        $navTitleContainer.css({
+          "opacity": "1",
+          "color": "#000"
+        })
+        $navTitle.css({
+          "display": "inherit"
+        })
+      }
+
+    } else {
+      // When Scroll is less than 200px
+      // Small Media < 768px
+      if (window.matchMedia('(max-width: 767px)').matches) {
+
+        $navFixed.css({
+          'background': 'rgba(255, 255, 255, 0.0)'
+        })
+        $navListItem.css({
+          'color': '#FFF',
+          'text-shadow': '1px 1px 1px rgba(0, 0, 0, 0.8)'
+        })
+        $navMain.addClass('nav-center')
+        $navTitleContainer.css({
+          "opacity": "0",
+          "color": "#000"
+        })
+
+      } else {
+        // Larger media > 768px
+        $navFixed.css({
+          'background': 'rgba(255, 255, 255, 0.0)'
+        })
+        $navListItem.css({
+          'color': '#FFF',
+          'text-shadow': '1px 1px 1px rgba(0, 0, 0, 0.8)'
+        })
+        $navMain.removeClass('nav-to-right').addClass('nav-center')
+        $navTitleContainer.css({
+          "opacity": "0",
+          "color": "#FFF"
+        })
+        $navTitle.fadeOut(300)
+      }
+
+    }
+  });
+
+  //////////////
+  // Functions
+  /////////////
+
+  // smoothScroll
+  function smoothScroll(thisObj) {
+    var $el = thisObj
+    var id = $el.attr('href')
+
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - $('nav').outerHeight()
+    }, 400)
+
+    return
+  }
+
+  // scrollApplyHighlight
+  function scrollApplyHighlight (thisObj, y) {
+    var $thisParent = thisObj.parent()
+
+    // If Scroll has reached a page section (by #id), apply .active class to corresponding link element
+    if (y >= $(thisObj.attr('href')).offset().top - 100) {
+      $allNavLinks.not(thisObj).parent().removeClass('active')
+      $thisParent.addClass('active')
+    } else {
+      $thisParent.removeClass('active')
+    }
+
+    return
+  }
+
+});
